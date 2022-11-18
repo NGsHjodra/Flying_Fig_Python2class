ddf216e219cfdda61b16034f118625deac052641
Niyakit <33505807+Niyakit@users.noreply.github.com>
2022-11-18 12:25:12 +0700

    finished product

--- Class.py
+++ Class.py
@@ -5,27 +5,27 @@ class Figure():
     def __init__(self) -> None:
         self.dx = 0
         self.dy = 0
+        self.x = 0
+        self.y = 0
         self.color = "red"
+
+    def update(self, dt):
+        self.x +=  self.dx * dt
+        self.y +=  self.dy * dt
 
 class Ball(Figure):
     def __init__(self) -> None:
         super().__init__()
-        self.x = 0
-        self.y = 0
         self.r = 0
-    
-    def update(self, dt):
-        self.x += self.x + self.dx * dt
-        self.y += self.y + self.dy * dt
 
     def collide(self, w, h):
-        if self.x >= w-self.r/2 or self.x <= self.r/2:
+        if self.x+self.r/2 >= w or self.x-self.r/2 <= 0:
             self.dx = -self.dx
-            self.dy = self.dy + random.uniform(-1,1)
+            self.dy = self.dy + random.uniform(-0.1,0.1)
             
-        if self.y >= h-self.r/2 or self.y <= self.r/2:
+        if self.y+self.r/2 >= h or self.y-self.r/2 <= 0:
             self.dy = -self.dy
-            self.dx = self.dx + random.uniform(-1,1)
+            self.dx = self.dx + random.uniform(-0.1,0.1)
 
     def draw(self, surf):
         pygame.draw.circle(surf, self.color, (self.x, self.y), self.r)
@@ -33,26 +33,17 @@ class Ball(Figure):
 class Rectangle(Figure):
     def __init__(self) -> None:
         super().__init__()
-        self.x1 = 0
-        self.x2 = 0
-        self.y1 = 0
-        self.y2 = 0
         self.w = 0
-
-    def update(self, dt):
-        self.x1 += self.x1 + self.dx * dt
-        self.y1 += self.y1 + self.dy * dt
-        self.x2 += self.x2 + self.dx * dt
-        self.y2 += self.y2 + self.dy * dt
+        self.h = 0
 
     def collide(self, w, h):
-        if self.x1 >= w or self.x2 <= 0:
+        if self.x+self.w >= w or self.x <= 0:
             self.dx = -self.dx
-            self.dy = self.dy + random.uniform(-1,1)
+            self.dy = self.dy + random.uniform(-0.1,0.1)
             
-        if self.y1 >= h or self.y2 <= 0:
+        if self.y+self.h >= h or self.y <= 0:
             self.dy = -self.dy
-            self.dx = self.dx + random.uniform(-1,1)
+            self.dx = self.dx + random.uniform(-0.1,0.1)
 
     def draw(self, surf):
-        pygame.draw.rect(surf, self.color, [self.x1, self.x2, self.y1, self.y2])
+        pygame.draw.rect(surf, self.color, [self.x, self.y, self.w, self.h])
--- main.py
+++ main.py
@@ -16,21 +16,9 @@ pygame.display.set_mode(window_size)
 
 done = False
 
-Balls = []
-
-for i in range(4):
-    ball = Ball()
-    ball.x = w/2 + random.uniform(-5,5)
-    ball.y = h/2 + random.uniform(-5,5)
-    ball.r = random.randint(40,60)
-    ball.dx = 0.5 + random.uniform(-0.1,0.1)
-    ball.dy = 0.5 + random.uniform(-0.1,0.1)
-    ball.color = random.choice(['red','blue','yellow'])
-    Balls.append(ball)
-
 Figs = []
 
-for i in range(4):
+for i in range(6):
     ball = Ball()
     ball.x = w/2 + random.uniform(-5,5)
     ball.y = h/2 + random.uniform(-5,5)
@@ -40,14 +28,14 @@ for i in range(4):
     ball.color = random.choice(['red','blue','yellow'])
     Figs.append(ball)
 
-for i in range(4):
+for i in range(6):
     rect = Rectangle()
-    rect.x1 = w/2 + random.uniform(-50,-45)
-    rect.y1 = h/2 + random.uniform(-50,-45)
-    rect.x2 = w/2 + random.uniform(45,50)
-    rect.y2 = h/2 + random.uniform(45,50)
-    rect.dx = 0.5 + random.uniform(-0.1,0.1)
-    rect.dy = 0.5 + random.uniform(-0.1,0.1)
+    rect.x = w/2 + random.uniform(-10, -5)
+    rect.y = rect.x
+    rect.w = 50 + random.randint(-10,10)
+    rect.h = 50 + random.randint(-10,10)
+    rect.dx = 0.5 + random.uniform(-0.4,0.4)
+    rect.dy = 0.5 + random.uniform(-0.4,0.4)
     rect.color = random.choice(['red','blue','yellow'])
     Figs.append(rect)
 
@@ -61,20 +49,12 @@ while not done:
         if event.type == pygame.QUIT: done = True
 
     surf = pygame.display.get_surface()
-    #for fig in Figs:
-    #    fig.draw(surf)
-
-    for ball in Balls:
-        ball.draw(surf)  
-
+    for fig in Figs:
+        fig.draw(surf)
     pygame.display.flip()
 
-    #for fig in Figs:
-    #    fig.update(dt)
-    #    fig.collide(w, h)
-
-    for ball in Balls:
-        ball.update(dt)
-        ball.collide(w, h)
+    for fig in Figs:
+        fig.update(dt)
+        fig.collide(w, h)
 
 pygame.quit()
